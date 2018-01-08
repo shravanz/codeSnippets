62d608d0b086a1aa916f90e85e620ed94b699c3a
Shravan Zolgikar <shravan.zolgikar@innoflexion.com>
2018-01-08 18:14:53 +0530

    added snippets

--- NodejsRealtedSnippets.js
+++ NodejsRealtedSnippets.js
@@ -109,24 +109,24 @@ exports.addPledgeDetails = function (req, cb) {
 
 //--------------------------------------------DB_to_File.js----------------------------------------------------------
 
-exports.getUserDetail = function (req,cb) {
-    User.find({isActive: true})
+exports.getUserDetail = function (req, cb) {
+    User.find({ isActive: true })
         .select({
-            '_id':1,
-            'userName':1,
-            'firstName':1,
-            'lastName':1
+            '_id': 1,
+            'userName': 1,
+            'firstName': 1,
+            'lastName': 1
         })
-        .exec(function (err,data) {
+        .exec(function (err, data) {
             //console.log(data);
-            fs.writeFileSync("./uploads/UserDetaInfo", JSON.stringify(data,null,4),'utf-8', function(err) {
-                if(err) {
+            fs.writeFileSync("./uploads/UserDetaInfo", JSON.stringify(data, null, 4), 'utf-8', function (err) {
+                if (err) {
                     return console.log(err);
                 }
- 
+
                 console.log("The file was saved!");
             });
-            return cb(err,data);
+            return cb(err, data);
         })
 };
 //Output
@@ -134,3 +134,141 @@ exports.getUserDetail = function (req,cb) {
     firstName: 'F_Name',
     userName: 'zyx@innoflexion.com' }
 */
+
+//=============================Braintree Payment Gateway Integration================================================
+
+//----------------------------------Braintree Implemntation.js------------------------------------------------------
+
+// assumes the following:
+// 1) First a client issues a request to my server for the client token
+// 2) My server responds with a token generated using my Braintree credentials (merchantId, public/private key)
+// 3) The client passes that token, along with the payment form data to the Braintree server
+// 4) The Braintree server responds with an authorization token (payment nonce)
+// 5) The client sends the payment nonce back to the server
+// 6) The server uses that nonce and the amount specified by the client to realize the transaction
+// 7) Braintree server is notified about the new transaction
+
+
+//======================BRAINTREE SANDBOX API===================================
+// var gateway = braintree.connect({
+//   environment: braintree.Environment.Sandbox,
+//   merchantId: 'r4d57q34dv8chxbr',
+//   publicKey: 'f4jgvnsk9by7s2wk',
+//   privateKey: '1ca957491840fa299f4ea39f614fd58a'
+// });
+//==============================================================================
+
+
+
+//=========================BRAINTREE MODEL======================================
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var paymentSchema = new Schema({
+    name: { type: String, required: [true, 'plz enter the name'] },
+    transactionID: { type: String, required: false },
+    amount: { type: String, required: [true, 'plz enter the amount'] },
+    paymentNonce: { type: String, required: [true, 'plz enter nonceString'] },
+    createdDate: { type: Date, required: true, default: Date.now },
+    modifiedDate: { type: Date, required: false }
+});
+
+// paymentSchema.plugin(uniqueValidator);
+// paymentSchema.plugin(deepPopulate);
+module.exports = mongoose.model('payment', paymentSchema);
+
+
+
+//=====================BRAINTREE SERVICES=======================================
+var Payment = require('../models/payment');
+var braintree = require('braintree');
+
+var gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    merchantId: 'xxx',
+    publicKey: 'yyy',
+    privateKey: 'zzz'
+});
+
+exports.addPayment = function (req, cb) {
+    var paymentData = new Payment();
+    paymentData.name = req.body.name;
+    paymentData.amount = req.body.amount;
+    paymentData.paymentNonce = req.body.paymentNonce;
+
+    gateway.transaction.sale({
+        amount: paymentData.amount,
+        paymentMethodNonce: paymentData.paymentNonce,
+        options: {
+            submitForSettlement: true
+        }
+    }, function (err, result) {
+        if (err || !result) {
+            return cb(err, result, null);
+        }
+        if (result.success)
+            paymentData.transactionID = result.transaction.id;
+        paymentData.save(function (err, response) {
+            return cb(err, response)
+        });
+    });
+    //save and check error
+    // paymentData.save(function (err,response) {
+    //     return cb(err,response)
+    // });
+};
+
+exports.getClientToken = function (req, cb) {
+    gateway.clientToken.generate({}, function (err, response) {
+        return cb(err, response.clientToken);
+    });
+};
+//=====================BRAINTREE ROUTE==========================================
+
+var express = require('express');
+var router = express.Router();
+var paymentSvc = require('../services/payment');
+var messages = require('../Handlers/msg_handler');
+router
+    .post('/addPayment', function (req, res, next) {
+
+        paymentSvc.addPayment(req, function (err, result) {
+
+            if (err) {
+
+                res.json({ error: true, message: messages.RECORD_ERROR, description: '', data: err });
+
+            } else {
+
+                res.json({ error: false, message: messages.RECORD_CREATED, description: '', data: result });
+
+            }
+
+        });
+
+    })
+
+    .get('/ClientToken', function (req, res, next) {
+
+        paymentSvc.getClientToken(req, function (err, result) {
+
+            if (err) {
+
+                res.json({ error: true, message: messages.RECORD_ERROR, description: '', data: err });
+
+            } else {
+
+                var response = result ?
+                    {
+                        error: false,
+                        message: messages.GET_ALL_RECORDS,
+                        description: '',
+                        data: result
+                    } : { error: true, message: messages.NO_RECORD, description: '', data: {} };
+
+            }
+            res.json(response);
+        });
+
+    });
+module.exports = router;
