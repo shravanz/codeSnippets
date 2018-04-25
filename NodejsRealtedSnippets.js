6ff9d8513a4b47f36225c1cbe69de3eb2a61070e
shravanz <shravanzolgikar@yahoo.in>
2018-04-25 15:01:33 +0530

    added snippet

--- NodejsRealtedSnippets.js
+++ NodejsRealtedSnippets.js
@@ -2,39 +2,69 @@
 
 //-----------------------getuserDataFromGameplay.js----------------------------------------------------------------
 
-exports.userData = function (req, cb) {
-    GamePlay.find({ "createdDate": { $gte: new Date(2017, 10, 19), $lte: new Date(2018, 03, 24) } }).deepPopulate('event team team.teamCaptain')
-        .exec(function (err, data) {
-            var array = [];
+exports.userData = function(req, cb) {
+  GamePlay.find({
+    createdDate: { $gte: new Date(2017, 10, 19), $lte: new Date(2018, 03, 24) }
+  })
+    .deepPopulate("event team team.teamCaptain")
+    .exec(function(err, data) {
+      var array = [];
 
-            data.forEach(function (arr) {
-                array.push({ "UserName": arr.team.teamCaptain.userName, "FirstName": arr.team.teamCaptain.firstName, "LastName": arr.team.teamCaptain.lastName, "EventTitle": arr.event.eventTitle });
+      data.forEach(function(arr) {
+        array.push({
+          UserName: arr.team.teamCaptain.userName,
+          FirstName: arr.team.teamCaptain.firstName,
+          LastName: arr.team.teamCaptain.lastName,
+          EventTitle: arr.event.eventTitle
+        });
+      });
+      console.log(array.length);
+      // console.log(data);
+      return cb(err, array);
+    });
+};
 
-            });
-            console.log(array.length);
-            // console.log(data);
-            return cb(err, array)
-        })
+//=======================Code For getting the user data for a particular event (KWIZFUN-REQ) ==============================
+exports.getUserBasedOnEvent = function(req, cb) {
+  GamePlay.find({ event: req.params._id })
+    .lean()
+    .deepPopulate("event team team.teamCaptain")
+    .exec(function(err, data) {
+      var array = [];
+      data.forEach(function(arr) {
+        array.push({
+          UserName: arr.team.teamCaptain.userName,
+          FirstName: arr.team.teamCaptain.firstName,
+          TeamName: arr.team.teamName,
+          EventTitle: arr.event.eventTitle
+        });
+      });
+      console.log(array.length);
+      return cb(err, array);
+    });
 };
 
 //=======================Code For Storing HTTP Images to Our Node JS Sever===================================
 
 //-----------------------HttpImg2NodeServer.js----------------------------------------------------------------
 
-var download = function (uri, filename, callback) {
-    request.head(uri, function (err, res, body) {
-        console.log('content-type:', res.headers['content-type']);
-        console.log('content-length:', res.headers['content-length']);
+var download = function(uri, filename, callback) {
+  request.head(uri, function(err, res, body) {
+    console.log("content-type:", res.headers["content-type"]);
+    console.log("content-length:", res.headers["content-length"]);
 
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
+    request(uri)
+      .pipe(fs.createWriteStream(filename))
+      .on("close", callback);
+  });
 };
-download(offerItem.largeImageUrl, './uploads/google.png' + Date.now(), function () {
-    console.log('done');
-});
-
-
-
+download(
+  offerItem.largeImageUrl,
+  "./uploads/google.png" + Date.now(),
+  function() {
+    console.log("done");
+  }
+);
 
 //=====This Code Illustrate the How we can Achieve (JSON/Obj/String) can be encrypted Using Node_Modules========
 
@@ -43,32 +73,30 @@ download(offerItem.largeImageUrl, './uploads/google.png' + Date.now(), function 
 //To Install Node_modules just Type in Your terminal
 //npm install simple-encryptor --save
 
-var key = 'Any Random String Which should be 16bytes long And No One should know,Expect You!!!';
+var key =
+  "Any Random String Which should be 16bytes long And No One should know,Expect You!!!";
 
-var encryptor = require('simple-encryptor')(key);
+var encryptor = require("simple-encryptor")(key);
 
-//let consider we have a Object that has to be  encrypted in the Response  
+//let consider we have a Object that has to be  encrypted in the Response
 var Ashwini = {
-    name: 'ashwini',
-    age: '25',
-    company: 'JCPenny'
-}
-
+  name: "ashwini",
+  age: "25",
+  company: "JCPenny"
+};
 
 var encrypted = encryptor.encrypt(Ashwini);
 var decrypted = encryptor.decrypt(encrypted);
 
 //We can also use String to encrypt
-var encryptedString = encryptor.encrypt('Ashwini');
+var encryptedString = encryptor.encrypt("Ashwini");
 
-//The Console Output Will Print something Gibreshhh!! 
+//The Console Output Will Print something Gibreshhh!!
 console.log(encrypted);
 console.log(encryptedString);
 
-//To Get Back The Orginal Object 
+//To Get Back The Orginal Object
 console.log(decrypted);
-
-
 
 //==========================A node-Js Npm Utilization For Generating Random String==============================
 
@@ -76,77 +104,83 @@ console.log(decrypted);
 
 var randomstring = require("randomstring");
 var no = randomstring.generate({
-    charset: 'abcdefghijklmnopqrstuvwxyz123456789',
-    length: 6,
-
+  charset: "abcdefghijklmnopqrstuvwxyz123456789",
+  length: 6
 });
 console.log(no);
 
 //Output --> b7m5q1
 
-
 //=====================Service FUnction on PledgeForm with email integration For trivia==========================
 
 //-------------------------------Trivia Pledge Form.js------------------------------------------------------------
 
-exports.addPledgeDetails = function (req, cb) {
+exports.addPledgeDetails = function(req, cb) {
+  var userId = req.body.user;
+  var email = req.body.email;
+  var amount = req.body.amount;
 
-    var userId = req.body.user;
-    var email = req.body.email;
-    var amount = req.body.amount;
+  User.findById(userId, function(err, result) {
+    if (err || !result) {
+      return cb(err, "UserErr", null);
+    }
+    var pledge = new Pledge();
+    pledge.event = req.body.event;
+    pledge.user = userId;
+    pledge.email = email;
+    pledge.amount = amount;
 
-    User.findById(userId, function (err, result) {
-        if (err || !result) { return cb(err, 'UserErr', null); }
-        var pledge = new Pledge();
-        pledge.event = req.body.event;
-        pledge.user = userId;
-        pledge.email = email;
-        pledge.amount = amount;
+    var sendMail = new sendGrid("sendgriduserName", "sendgridPassword");
+    sendMail.send(
+      {
+        to: email,
+        from: sendGridConfig.From_Address,
+        subject: "PledgeForm",
+        text:
+          "ThankYou For submitting your pledge with amount of " +
+          amount +
+          "$ we appreciate your concern!"
+      },
+      function(sendData) {
+        //res.json({error: false, message: messages.SEND_MAIL, description: '', data: {}});
+      }
+    );
 
-        var sendMail = new sendGrid('sendgriduserName', 'sendgridPassword');
-        sendMail.send({
-            to: email,
-            from: sendGridConfig.From_Address,
-            subject: 'PledgeForm',
-            text: 'ThankYou For submitting your pledge with amount of ' + amount + '$ we appreciate your concern!'
-        }, function (sendData) {
-
-            //res.json({error: false, message: messages.SEND_MAIL, description: '', data: {}});
-
-        });
-
-        // save and check for errors
-        pledge.save(function (err) {
-
-            return cb(err, null, pledge);
-        });
-
+    // save and check for errors
+    pledge.save(function(err) {
+      return cb(err, null, pledge);
     });
+  });
 };
 
 //===========A Node JS Server Where it Fetches Data From the mongoDb Json Data And Write it in a text file===========
 
 //--------------------------------------------DB_to_File.js----------------------------------------------------------
 
-exports.getUserDetail = function (req, cb) {
-    User.find({ isActive: true })
-        .select({
-            '_id': 1,
-            'userName': 1,
-            'firstName': 1,
-            'lastName': 1
-        })
-        .exec(function (err, data) {
-            //console.log(data);
-            fs.writeFileSync("./uploads/UserDetaInfo", JSON.stringify(data, null, 4), 'utf-8', function (err) {
-                if (err) {
-                    return console.log(err);
-                }
+exports.getUserDetail = function(req, cb) {
+  User.find({ isActive: true })
+    .select({
+      _id: 1,
+      userName: 1,
+      firstName: 1,
+      lastName: 1
+    })
+    .exec(function(err, data) {
+      //console.log(data);
+      fs.writeFileSync(
+        "./uploads/UserDetaInfo",
+        JSON.stringify(data, null, 4),
+        "utf-8",
+        function(err) {
+          if (err) {
+            return console.log(err);
+          }
 
-                console.log("The file was saved!");
-            });
-            return cb(err, data);
-        })
+          console.log("The file was saved!");
+        }
+      );
+      return cb(err, data);
+    });
 };
 //Output
 /*{ _id: 59d5cc1eec9c601c3472811c,
@@ -167,7 +201,6 @@ exports.getUserDetail = function (req, cb) {
 // 6) The server uses that nonce and the amount specified by the client to realize the transaction
 // 7) Braintree server is notified about the new transaction
 
-
 //======================BRAINTREE SANDBOX API===================================
 // var gateway = braintree.connect({
 //   environment: braintree.Environment.Sandbox,
@@ -177,117 +210,121 @@ exports.getUserDetail = function (req, cb) {
 // });
 //==============================================================================
 
-
-
 //=========================BRAINTREE MODEL======================================
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var paymentSchema = new Schema({
-    name: { type: String, required: [true, 'plz enter the name'] },
-    transactionID: { type: String, required: false },
-    amount: { type: String, required: [true, 'plz enter the amount'] },
-    paymentNonce: { type: String, required: [true, 'plz enter nonceString'] },
-    createdDate: { type: Date, required: true, default: Date.now },
-    modifiedDate: { type: Date, required: false }
+  name: { type: String, required: [true, "plz enter the name"] },
+  transactionID: { type: String, required: false },
+  amount: { type: String, required: [true, "plz enter the amount"] },
+  paymentNonce: { type: String, required: [true, "plz enter nonceString"] },
+  createdDate: { type: Date, required: true, default: Date.now },
+  modifiedDate: { type: Date, required: false }
 });
 
 // paymentSchema.plugin(uniqueValidator);
 // paymentSchema.plugin(deepPopulate);
-module.exports = mongoose.model('payment', paymentSchema);
-
-
+module.exports = mongoose.model("payment", paymentSchema);
 
 //=====================BRAINTREE SERVICES=======================================
-var Payment = require('../models/payment');
-var braintree = require('braintree');
+var Payment = require("../models/payment");
+var braintree = require("braintree");
 
 var gateway = braintree.connect({
-    environment: braintree.Environment.Sandbox,
-    merchantId: 'xxx',
-    publicKey: 'yyy',
-    privateKey: 'zzz'
+  environment: braintree.Environment.Sandbox,
+  merchantId: "xxx",
+  publicKey: "yyy",
+  privateKey: "zzz"
 });
 
-exports.addPayment = function (req, cb) {
-    var paymentData = new Payment();
-    paymentData.name = req.body.name;
-    paymentData.amount = req.body.amount;
-    paymentData.paymentNonce = req.body.paymentNonce;
+exports.addPayment = function(req, cb) {
+  var paymentData = new Payment();
+  paymentData.name = req.body.name;
+  paymentData.amount = req.body.amount;
+  paymentData.paymentNonce = req.body.paymentNonce;
 
-    gateway.transaction.sale({
-        amount: paymentData.amount,
-        paymentMethodNonce: paymentData.paymentNonce,
-        options: {
-            submitForSettlement: true
-        }
-    }, function (err, result) {
-        if (err || !result) {
-            return cb(err, result, null);
-        }
-        if (result.success)
-            paymentData.transactionID = result.transaction.id;
-        paymentData.save(function (err, response) {
-            return cb(err, response)
-        });
-    });
-    //save and check error
-    // paymentData.save(function (err,response) {
-    //     return cb(err,response)
-    // });
+  gateway.transaction.sale(
+    {
+      amount: paymentData.amount,
+      paymentMethodNonce: paymentData.paymentNonce,
+      options: {
+        submitForSettlement: true
+      }
+    },
+    function(err, result) {
+      if (err || !result) {
+        return cb(err, result, null);
+      }
+      if (result.success) paymentData.transactionID = result.transaction.id;
+      paymentData.save(function(err, response) {
+        return cb(err, response);
+      });
+    }
+  );
+  //save and check error
+  // paymentData.save(function (err,response) {
+  //     return cb(err,response)
+  // });
 };
 
-exports.getClientToken = function (req, cb) {
-    gateway.clientToken.generate({}, function (err, response) {
-        return cb(err, response.clientToken);
-    });
+exports.getClientToken = function(req, cb) {
+  gateway.clientToken.generate({}, function(err, response) {
+    return cb(err, response.clientToken);
+  });
 };
 //=====================BRAINTREE ROUTE==========================================
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var paymentSvc = require('../services/payment');
-var messages = require('../Handlers/msg_handler');
+var paymentSvc = require("../services/payment");
+var messages = require("../Handlers/msg_handler");
 router
-    .post('/addPayment', function (req, res, next) {
-
-        paymentSvc.addPayment(req, function (err, result) {
-
-            if (err) {
-
-                res.json({ error: true, message: messages.RECORD_ERROR, description: '', data: err });
-
-            } else {
-
-                res.json({ error: false, message: messages.RECORD_CREATED, description: '', data: result });
-
-            }
-
+  .post("/addPayment", function(req, res, next) {
+    paymentSvc.addPayment(req, function(err, result) {
+      if (err) {
+        res.json({
+          error: true,
+          message: messages.RECORD_ERROR,
+          description: "",
+          data: err
         });
-
-    })
-
-    .get('/ClientToken', function (req, res, next) {
-
-        paymentSvc.getClientToken(req, function (err, result) {
-
-            if (err) {
-
-                res.json({ error: true, message: messages.RECORD_ERROR, description: '', data: err });
-
-            } else {
-
-                var response = result ?
-                    {
-                        error: false,
-                        message: messages.GET_ALL_RECORDS,
-                        description: '',
-                        data: result
-                    } : { error: true, message: messages.NO_RECORD, description: '', data: {} };
-
-            }
-            res.json(response);
+      } else {
+        res.json({
+          error: false,
+          message: messages.RECORD_CREATED,
+          description: "",
+          data: result
         });
-
+      }
     });
+  })
+
+  .get("/ClientToken", function(req, res, next) {
+    paymentSvc.getClientToken(req, function(err, result) {
+      if (err) {
+        res.json({
+          error: true,
+          message: messages.RECORD_ERROR,
+          description: "",
+          data: err
+        });
+      } else {
+        var response = result
+          ? {
+              error: false,
+              message: messages.GET_ALL_RECORDS,
+              description: "",
+              data: result
+            }
+          : {
+              error: true,
+              message: messages.NO_RECORD,
+              description: "",
+              data: {}
+            };
+      }
+      res.json(response);
+    });
+  });
 module.exports = router;
